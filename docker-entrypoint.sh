cf9ce3ead5d8578b69c11d19776bcec531ad69c3
YuYang <yang.lights@hotmail.com>
2017-08-31 09:54:02 +0800

    add config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+exec su-exec ghost config --no-prompt --url "$HOST_URL"
+
 # allow the container to be started with `--user`
 if [[ "$*" == node*current/index.js* ]] && [ "$(id -u)" = '0' ]; then
 	chown -R node "$GHOST_CONTENT"
